3142978ea2aa4774ded6207e82d54aadbb681964
ludovic <ludovic.s@protonmail.com>
2019-03-28 13:46:30 +0100

    Ajout de la boucle de test pour une position ok dans le mastermind

--- src/com/main/Mastermind.java
+++ src/com/main/Mastermind.java
@@ -31,8 +31,10 @@ public class Mastermind {
         int longNbAleaConf = 4; //todo A coder dans le fichier de conf //Longueur de la combinaison
         int nombreTourConf = 10; //todo A coder dans le fichier de conf//nombre de tour possible
         int nombreChiffresUtilisables = 4; //todo A coder dans le fichier de conf//nombre de chiffres utilisables
-        int i = 0;//Compteur de nombre de tour
-        int tentativeCombinaison ;
+        int numeroTour = 0;//Compteur de nombre de tour
+        int nombrePositionOk = 0 ;
+
+        String tentativeCombinaison ;
 
         StringBuilder combinaisonSecrete = new StringBuilder();//Combinaison secrète généré par l'ordinateur
 
@@ -41,15 +43,23 @@ public class Mastermind {
         //L'ordinateur doit générer la solution
         combinaisonSecrete = Tools.geneNbAlea(longNbAleaConf, 1, nombreChiffresUtilisables);
         System.out.println(combinaisonSecrete);
+
         //Demande au joueur de saisir sa tentative pour trouver la combinaison secrète
-        i++;
-        Tools.affichageTour(i, nombreTourConf);
+        numeroTour++;
+        Tools.affichageTour(numeroTour, nombreTourConf);
         System.out.println("Veuillez saisir une combinaison (Chiffre de 1 à " +nombreChiffresUtilisables+ ")");
-        tentativeCombinaison = sc.nextInt();
+        tentativeCombinaison = sc.next();
 
         //Comparer la réponse de l'utilisateur à la solutions
         //Générer la réponse de l'ordinateur
-        //Afficher la réponse de l'ordinateur
+
+        for (int i = 0; i < longNbAleaConf; i++){
+            if (combinaisonSecrete.toString().charAt(i) == tentativeCombinaison.charAt(i)){
+                nombrePositionOk++;
+            }
+        }
+        System.out.println("Vous avez " +nombrePositionOk+ " pion en bonne position");
+        //Afficher la réponse de l'ordinateure
         //En fonction de la réponse, nouveau tour ou fin de jeu
         //Message pour rejouer, changer de jeu ou quitter
     }
