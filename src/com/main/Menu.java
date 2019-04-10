ed27d566a0900379890a237475364af663e5033d
ludovic <ludovic.s@protonmail.com>
2019-04-10 15:19:17 +0200

    Ajout de la méthode saisieNuméros dans le menu de séléction du jeu et du mode

--- src/com/main/Menu.java
+++ src/com/main/Menu.java
@@ -16,28 +16,28 @@ public class Menu {
         PlusOuMoins plusOuMoins = new PlusOuMoins(longNbAlea, nombreDeTour, devMode);
         Mastermind mastermind = new Mastermind(longNbAlea, nombreDeTour, nombreChiffreUtilisables, devMode);
 
-        int choixJeu;
-        int choixMode;
+        String choixJeu;
+        String choixMode;
 
         System.out.println("Veuillez choisir votre jeux");
         System.out.println("1: Recherche +/-");
         System.out.println("2: Mastermind");
         Scanner sc = new Scanner(System.in);
-        choixJeu = sc.nextInt();
+        choixJeu = Tools.saisieNuméros(1);
         switch (choixJeu) {
-            case 1:
+            case "1":
                 System.out.println("Veuilez choisir votre mode de jeu. 1 - Challenger, 2 - Défenseur, 3 - Duel");
-                choixMode = sc.nextInt();
+                choixMode = Tools.saisieNuméros(1);
                 switch (choixMode) {
-                    case 1:
+                    case "1":
                         System.out.println("Bienvenue dans le +/-, mode Challenger !");
                         plusOuMoins.plusOuMoinsChallenger();
                         break;
-                    case 2:
+                    case "2":
                         System.out.println("Bienvenue dans le +/-, mode Défenseur !");
                         plusOuMoins.plusOuMoinsDefenseur();
                         break;
-                    case 3:
+                    case "3":
                         System.out.println("Bievenue dans le +/-, mode duel !");
                         plusOuMoins.plusOuMoinsDuel();
                         break;
@@ -45,19 +45,19 @@ public class Menu {
                         gameChoice();
                 }
                 break;
-            case 2:
+            case "2":
                 System.out.println("Veuilez choisir votre mode de jeu. 1 - Challenger, 2 - Défenseur, 3 - Duel");
-                choixMode = sc.nextInt();
+                choixMode = Tools.saisieNuméros(1);
                 switch (choixMode) {
-                    case 1:
+                    case "1":
                         System.out.println("Bienvenue dans le Mastermind, mode Challenger !");
                         mastermind.mastermindChallenger();
                         break;
-                    case 2:
+                    case "2":
                         System.out.println("Bienvenue dans le Mastermind, mode Défenseur !");
                         mastermind.mastermindDefenseur();
                         break;
-                    case 3:
+                    case "3":
                         System.out.println("Bievenue dans le Mastermind, mode duel !");
                         mastermind.mastermindDuel();
                         break;
